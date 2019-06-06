781a856e3aed698effd58335e806fa1f38a1733b
Freitas29 <arao.freitas@hotmail.com>
2019-06-06 01:47:26 -0300

    route root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  root 'welcome#index'
+
   devise_for :users, path: 'auth', path_names: {sign_in: 'login', sign_out: 'logout', registration: 'register'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
