454771c709012608646ece8d0ef1d675a76db214
Freitas29 <arao.freitas@hotmail.com>
2019-06-08 03:01:01 -0300

    logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  
   resources :posts
   root 'welcome#index'
-
+  
   devise_for :users, path: 'auth', path_names: {sign_in: 'login', sign_out: 'logout', registration: 'register'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
